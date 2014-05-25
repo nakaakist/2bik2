4e0e5948f9a7f64a24ac5d1bc3d5ddfd124ea24f
nariyuki <nariyuki@nariyukis-pc.local>
2014-05-25 18:30:51 +0900

    Initial Commit

---
+++ 2bik/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  2bik
+//
+//  Created by nariyuki on 5/25/14.
+//  Copyright (c) 2014 Nariyuki Saito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 2bik/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  2bik
+//
+//  Created by nariyuki on 5/25/14.
+//  Copyright (c) 2014 Nariyuki Saito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
